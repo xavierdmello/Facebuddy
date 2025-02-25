1c3f418ed88df42c662608e1ca44f580320d0925
William Wang <bwilliamwang@gmail.com>
2025-02-25 13:23:26 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,13 +32,13 @@ export default function Page() {
         </div>
       </section>
       <section className="templateSection flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-gray-100 px-2 py-4 md:grow">
-        <div className="flex h-[450px] w-[450px] max-w-full items-center justify-center rounded-xl bg-[#030712]">
+        {/* <div className="flex h-[450px] w-[450px] max-w-full items-center justify-center rounded-xl bg-[#030712]">
           <div className="rounded-xl bg-[#F3F4F6] px-4 py-[11px]">
             <p className="font-normal text-indigo-600 text-xl not-italic tracking-[-1.2px]">
               npm install @coinbase/onchainkit
             </p>
           </div>
-        </div>
+        </div> */}
         {address ? (
           <>
             <FaceDetection />
